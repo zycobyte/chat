61b524b874631b67d154c270d739e6d23096678a
Charlie <charlieferris15@gmail.com>
2017-11-11 00:52:24 +0000

    Add files via upload

--- resources/scripts/chats.js
+++ resources/scripts/chats.js
@@ -3,16 +3,20 @@ function onSend(message){
 	"{oAuth:key;username:name;content:message;}"
 	*/
 	
-	data = "\"{oAuth:"+readCookie("oAuth");+";username:"+readCookie("username");+";content:"+message+";}\""
+	data = "\"{oAuth:"+readCookie("oAuth")+";username:"+readCookie("username")+";content:"+message.value+";}\""
+	message.value = "";
 	
 	console.log("##SENDING##\n"+data);
 	
 }
 
-document.getElementById("messageArea")
-    .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("send").click();
-    }
-});
+
+function addListener(id, button){
+	document.getElementById(id)
+	    .addEventListener("keyup", function(event) {
+	    event.preventDefault();
+	    if (event.keyCode === 13) {
+	        document.getElementById(button).click();
+	    }
+	});
+}
