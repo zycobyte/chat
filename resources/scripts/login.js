5a289db3bb3fae49bfd43889f779ee6dae7b4a72
Charlie <charlieferris15@gmail.com>
2017-11-11 00:03:18 +0000

    Add files via upload

---
+++ resources/scripts/login.js
@@ -0,0 +1,39 @@
+var SERVER = "35.167.63.112";
+var PORT = 25006;
+
+function sendRequest(){
+	name = document.getElementById("name").value;
+	pass = document.getElementById("pass").value;
+	
+	message = "login " + name + " " + hash(pass);
+	
+	alert(message + " ###TODO LOGIN SCRIPTS HERE");
+	valid = true;
+	
+	if(valid){
+		createCookie("username", name, 10);
+		createCookie("oAuth", "null", 10);
+		window.location.href="chats.html";
+	}else{
+		//Something here
+	}
+}
+function hash(string){
+    var hash = 0;
+    if (string.length == 0) return hash;
+    for (i = 0; i < string.length; i++) {
+        char = string.charCodeAt(i);
+        hash = ((hash<<5)-hash)+char;
+        hash = hash & hash; // Convert to 32bit integer
+    }
+    return hash;
+}
+
+
+
+
+//createCookie("testCookie", "test", 2);
+//createCookie("q54etr", "test", 2);
+//createCookie("t", "test", 2);
+//createCookie("4tergf", "test", 2);
+//createCookie("eryt", "test", 2);
