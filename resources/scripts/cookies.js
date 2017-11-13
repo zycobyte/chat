87b96a466e262e59e09030be367b54976c10c383
Charlie <charlieferris15@gmail.com>
2017-11-13 00:07:58 +0000

    Update cookies.js

--- resources/scripts/cookies.js
+++ resources/scripts/cookies.js
@@ -5,7 +5,7 @@ function createCookie(name,value,days) {
         date.setTime(date.getTime() + (days*24*60*60*1000));
         expires = "; expires=" + date.toUTCString();
     }
-    document.cookie = name + "=" + value + expires + "; path=lavatheif.github.io/cookies";
+    document.cookie = name + "=" + value + expires + "; path=/";
 }
 
 function readCookie(name) {
