e2a220a565d622f9d6e2d2a1657cfd8d834faa1f
Charlie <charlieferris15@gmail.com>
2018-04-21 14:13:55 +0100

    WSS

--- res/js/Utils.js
+++ res/js/Utils.js
@@ -1,6 +1,6 @@
 pre = (window.location.hostname==="localhost"?"/Website":"");
 let server = (window.location.hostname==="localhost"?"ws://localhost":"wss://eiennochat.uk")+":2096/eiennosocket/data";
-let dataserver = (window.location.hostname==="localhost"?"ws://localhost":"wss://eiennochat.uk")+":2085/datasocket/data";
+let dataserver = (window.location.hostname==="localhost"?"ws://localhost":"wss://eiennochat.uk")+":2089/datasocket/data";
 
 allowStorage = (typeof(Storage) !== "undefined");
 messages = 0;
